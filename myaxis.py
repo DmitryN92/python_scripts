aa6ee134043acafeb35fb546d71e2852829379af
Dmitry9205 <85824857+DmitryN92@users.noreply.github.com>
2021-06-13 13:39:28 +0300

    Add files via upload

---
+++ myaxis.py
@@ -0,0 +1,31 @@
+"""
+Окно с осью Х и возможностью перемещаться
+по этой оси от -50 до 50
+"""
+
+from tkinter import *
+root = Tk()
+
+scl = Scale(root, label="X axis", tickinterval=10, resolution=1, length=300, from_=-50, to=50)
+var = IntVar()
+scl.config(variable=var)
+Label(root, text="Grafical calculator by Dmitry Pupkin (aka Inferno)").pack(side=TOP)
+
+scl.config(orient='horizontal')
+scl.pack(expand=YES,fill=X,anchor=CENTER)
+
+ent = Entry()
+ent.insert(0,'type exp here')
+ent.config(width=100)
+
+def mycalculator():
+	myexp = ent.get()
+	myexp = myexp.strip()
+	myresult = eval(myexp)
+	var.set(myresult)
+
+Button(root, text="Calculate", command=mycalculator).pack(side=BOTTOM)
+
+
+ent.pack(side=BOTTOM)
+root.mainloop()
